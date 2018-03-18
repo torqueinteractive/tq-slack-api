c37383ead4a1741d6d575d80217b6bfc0e7808b4
Jonathan Bowman <jonathanrbowman@me.com>
2018-03-18 19:40:04 -0400

    Added team id to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180318231821) do
+ActiveRecord::Schema.define(version: 20180318233924) do
 
   create_table "teams", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180318231821) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slack_user_name"
+    t.integer "team_id"
   end
 
 end
